a117dfd5f92dec691eb70876efdd0ff32398b36a
abghifareihand <info.abghifareihan@gmail.com>
2024-02-27 12:32:45 +0700

    add API UpdatePhoto UserController

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Actions\Fortify\PasswordValidationRules;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -79,7 +80,7 @@ class UserController extends Controller
                 'access_token' => $tokenResult,
                 'token_type' => 'Bearer',
                 'user' => $user
-            ]);
+            ], 'User Registered');
         } catch (Exception $error) {
             return ResponseFormatter::error([
                 'message' => 'Something went wrong',
@@ -96,9 +97,10 @@ class UserController extends Controller
 
     public function fetch(Request $request)
     {
-      return ResponseFormatter::success(
-        $request->user(), 'Get Profile Success'
-      );
+        return ResponseFormatter::success(
+            $request->user(),
+            'Get Profile Success'
+        );
     }
 
     public function updateProfile(Request $request)
@@ -109,5 +111,29 @@ class UserController extends Controller
         $user->update($data);
 
         return ResponseFormatter::success($user, 'Profile Update Success');
+    }
+
+    public function updatePhoto(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'file' => 'required|image|max:2048'
+        ]);
+
+        if ($validator->fails()) {
+            return ResponseFormatter::error([
+                'error' => $validator->errors()
+            ], 'Update Photo Failed', 401);
+        }
+
+        if ($request->file('file')) {
+            $file = $request->file->store('assets/user', 'public');
+
+            // Simpan foto ke database (url)
+            $user = Auth::user();
+            $user->profile_photo_path = $file;
+            $user->update();
+
+            return ResponseFormatter::success([$file], 'File Upload Success');
+        }
     }
 }
