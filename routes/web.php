8438d9ec734b41e00741a4b51e3e2cf6e7f3056e
abghifareihand <info.abghifareihan@gmail.com>
2024-02-29 23:38:00 +0700

    CRUD TransactionController

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\MidtransController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FoodController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,6 +28,8 @@ Route::prefix('dashboard')->middleware(['auth:sanctum', 'admin'])->group(functio
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('users', UserController::class);
     Route::resource('food', FoodController::class);
+
+    Route::resource('transaction', TransactionController::class);
 });
 
 // Midtrans Page Success, Unfinish, Error
